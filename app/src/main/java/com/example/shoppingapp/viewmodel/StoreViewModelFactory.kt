793f2385ae4ceaec7b8d64d6b670593cda81b5a1
Rajendhiran E <rajendhiran.easu@liveramp.com>
2025-08-05 17:22:37 +0530

    updated dependencies and agp

--- app/src/main/java/com/example/shoppingapp/viewmodel/StoreViewModelFactory.kt
+++ app/src/main/java/com/example/shoppingapp/viewmodel/StoreViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.shoppingapp.repository.StoreRepository
 
 class StoreViewModelFactory constructor(private val repository: StoreRepository): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(StoreViewModel::class.java)) {
             StoreViewModel(this.repository) as T
         } else {
